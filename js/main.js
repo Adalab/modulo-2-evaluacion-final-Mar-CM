77f349a3c8af8543f1446fee7805caedbcbb07c0
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-03-05 15:59:34 +0100

    Added LocalStorage

--- js/main.js
+++ js/main.js
@@ -2,23 +2,46 @@
 
 const nameInput = document.querySelector('.js-name');
 const buttonSearch = document.querySelector('.js-search');
+const error = document.querySelector('.js-error');
 const seriesList = document.querySelector('.js-list-series');
 const buttonReset = document.querySelector('.js-reset');
+const containerFavSeries = document.querySelector('.js-list-series-fav');
 
 
 
+
+//función de favoritos y guardar en localstorage
+function handleAddFav(event){
+    const favAnime = event.currentTarget.innerHTML;
+    containerFavSeries.innerHTML += `<li class= "fav-series">${favAnime}</li>`;
+    
+    //Obtenemos la lista de fav del local
+    const storedFavorites = localStorage.getItem('favoritos');
+    const favorites = storedFavorites ? JSON.parse(storedFavorites) : [];
+    //Agregamos nuevo fav al array
+    favorites.push(favAnime);
+    // Guardar la lista actualizada en el localStorage
+    localStorage.setItem('favoritos', JSON.stringify(favorites));
+}
+
+//función que busca las series, incluye condicional y favoritos ya que solo podemos añadir fav cuando se pintan en la web.
 function renderAnime(animesList){
-    seriesList.innerHTML= '';
+    console.log(animesList);
+    seriesList.innerHTML= 'Resultados';
     
     for (const anime of animesList){//Condicional, si la foto, es un url, cambiar por la otra foto, sino, dejarlo.
         if (anime.images.jpg.image_url === 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'){
             const imageUrl = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-            seriesList.innerHTML += `<li class= "search">${anime.title}<img src="${imageUrl}"></li>`;
+            seriesList.innerHTML += `<li class= "series js-seriesAnime" id="${anime.mal_id}">${anime.title}<img src="${imageUrl}"></li>`;
         } else {
-            seriesList.innerHTML += `<li class= "search">${anime.title}<img src="${anime.images.jpg.image_url}"></li>`;
+            seriesList.innerHTML += `<li class= "series js-seriesAnime" id="${anime.mal_id}">${anime.title}<img src="${anime.images.jpg.image_url}"></li>`;
         }
     }
-   
+
+    const seriesFav = document.querySelectorAll('.js-seriesAnime');
+    for (const favorite of seriesFav){
+        favorite.addEventListener('click', handleAddFav)
+    }
 }
 
 //Evento sobre botón buscar - pediremos al servidor datos al clickar buscar.
@@ -27,7 +50,7 @@ function handleSearch(event){
     const nameAnime = nameInput.value;
 
     if (nameAnime === "" ){
-        seriesList.innerHTML = `Por favor, introduce un nombre.`;
+        error.innerHTML = `Por favor, introduce un nombre.`;
     } else {
             // Petición al servidor de mostrar las series depende de lo q busque el usuario. He hecho esto, ya que si no pone nada, no se mostrará.
         fetch(`https://api.jikan.moe/v4/anime?q=${nameAnime}`)
@@ -36,10 +59,7 @@ function handleSearch(event){
             const animesList = data.data;
             renderAnime(animesList); //hemos sacado la función que nos mostrará la busqueda del input.
         })
-
     }
-
-   
 }
 
 //Evento sobre el botón reset - borrará todo el contenido
@@ -48,5 +68,5 @@ function handleReset(){
     const nameInput = "";
 }
 
-buttonReset.addEventListener('click', handleReset)
+buttonReset.addEventListener('click', handleReset);
 buttonSearch.addEventListener('click', handleSearch);
